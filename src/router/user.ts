651a08076d8ba2bf8c5b1cc29650355004cf732b
David Santos <89614359+DaviidSantos@users.noreply.github.com>
2023-03-23 16:59:53 -0300

    Delete User Route

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 
-import { getAllUsers } from '../controllers/user'
+import { deleteUser, getAllUsers } from '../controllers/user'
 import { isAuthenticated } from '../middlewares/index'
 
 export default (router: express.Router) => {
     router.get('/user', isAuthenticated, getAllUsers)
+    router.delete('/user/delete/:id', deleteUser)
 }
