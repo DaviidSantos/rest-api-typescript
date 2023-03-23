46aabb0f683e4627ba650319b7ff49a73a608c81
David Santos <89614359+DaviidSantos@users.noreply.github.com>
2023-03-23 17:16:47 -0300

    User Controller - Update User

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express from "express";
 
-import { deleteUser, getAllUsers } from '../controllers/user'
-import { isAuthenticated, isOwner } from '../middlewares/index'
+import { deleteUser, getAllUsers, updateUser } from "../controllers/user";
+import { isAuthenticated, isOwner } from "../middlewares/index";
 
 export default (router: express.Router) => {
-    router.get('/user', isAuthenticated, getAllUsers)
-    router.delete('/user/delete/:id', isAuthenticated, isOwner, deleteUser)
-}
+  router.get("/user", isAuthenticated, getAllUsers);
+  router.delete("/user/delete/:id", isAuthenticated, isOwner, deleteUser);
+  router.patch("/user/update/:id", isAuthenticated, isOwner, updateUser);
+};
